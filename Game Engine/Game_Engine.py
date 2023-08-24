343eaf692cc8ed988bcd8e8956e8cb23789b8bd6
Aar0n11 <monsterrmann@hotmail.com>
2023-08-24 13:52:55 -0700

    a

--- Game Engine/Game_Engine.py
+++ Game Engine/Game_Engine.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import os
 import json
 import time
@@ -13,24 +14,30 @@ dataFile = open(parent + '\settings.json')
 data = json.load(dataFile)
 
 processSpeed = settingsData['speed']
+print(processSpeed)
 
 position = [0, 0]
 
 def move(direction):
         if(direction == "forward"):
             position[1] = position[1] + 5
+            print(position)
+
             time.sleep(processSpeed)
 
         elif(direction == "backward"):
             position[1] = position[1] - 5
+            print(position)
             time.sleep(processSpeed)
 
         elif(direction == "left"):
             position[0] == position[0] - 5
+            print(position)
             time.sleep(processSpeed)
 
         elif(direction == "right"):
             position[0] == position[0] + 5
+            print(position)
             time.sleep(processSpeed)
 
 keyboard.add_hotkey(settingsData['leftKey'], lambda: move("left"))
@@ -39,4 +46,5 @@ keyboard.add_hotkey(settingsData['forwardKey'], lambda: move("forward"))
 keyboard.add_hotkey(settingsData['backwardKey'], lambda: move("backward"))
 
 while True:
-    move()
+    print("Hi")
+    time.sleep(0.1)
