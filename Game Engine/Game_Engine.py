fb20aa3ddeb4f1e172bd1b39d5e14409a539701e
Aar0n11 <monsterrmann@hotmail.com>
2023-08-24 11:04:34 -0700

    Added 0.1 sec delay between updates

--- Game Engine/Game_Engine.py
+++ Game Engine/Game_Engine.py
@@ -1,10 +1,14 @@
-import subprocess
+import time
 import keyboard
 
-def move(key):
-
+a = 0
 
 keyboard.add_hotkey("a", lambda: move("a"))
 keyboard.add_hotkey("d", lambda: move("d"))
 keyboard.add_hotkey("w", lambda: move("w"))
 keyboard.add_hotkey("s", lambda: move("s"))
+keyboard.add_hotkey("esc", a = 1)
+
+while(a != 1):
+    def move(key):
+        print(key)
