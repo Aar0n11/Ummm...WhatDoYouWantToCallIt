7bd8ed9b5103975b8137ac9950b7ad8a75559f20
Aar0n11 <monsterrmann@hotmail.com>
2023-08-24 11:05:58 -0700

    a

--- Game Engine/Game_Engine.py
+++ Game Engine/Game_Engine.py
@@ -3,12 +3,11 @@ import keyboard
 
 a = 0
 
+def move(key):
+        print(key)
+        time.sleep(0.1)
+
 keyboard.add_hotkey("a", lambda: move("a"))
 keyboard.add_hotkey("d", lambda: move("d"))
 keyboard.add_hotkey("w", lambda: move("w"))
 keyboard.add_hotkey("s", lambda: move("s"))
-keyboard.add_hotkey("esc", a = 1)
-
-while(a != 1):
-    def move(key):
-        print(key)
